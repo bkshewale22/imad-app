6039f0325f49bd4931b292fdfc94bb0219427676
bkshewale22 <36422804+bkshewale22@users.noreply.github.com>
2018-02-28 01:15:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/artical-1',function(req,res){
-   res.send('artical one is req ');
+   res.sendFile(path.join(__dirname, 'ui', 'artical-1.html'));
 });
 app.get('/artical-2',function(req,res){
    res.send('artical two is req ');
