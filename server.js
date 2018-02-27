95be74b538ba542add99408024d5d3773205d2de
bkshewale22 <36422804+bkshewale22@users.noreply.github.com>
2018-02-28 02:08:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,54 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articalOne ={
+    title:'artical one',
+    cont:`<p>
+            this is my first contant of web app.this is my first contant of web app.this is my first contant of web app.this is my first contant of web app.this is my first contant of web app.this is my first contant of web app.this is my first contant of web app.this is my first contant of web app.this is my first contant of web app.this is my first contant of web app.
+            </p>
+        <p>
+            this is my first contant of web app.this is my first contant of web app.this is my first contant of web app.this is my first contant of web app.this is my first contant of web app.this is my first contant of web app.this is my first contant of web app.this is my first contant of web app.this is my first contant of web app.this is my first contant of web app.
+        </p>
+        <p>
+            this is my first contant of web app.this is my first contant of web app.this is my first contant of web app.this is my first contant of web app.this is my first contant of web app.this is my first contant of web app.this is my first contant of web app.this is my first contant of web app.this is my first contant of web app.this is my first contant of web app.
+        </p>`,
+        hed: 'this first artical',
+        date: 'feb 28,2018'
+    
+};
+function CreateTemlate(data){
+    var title=data.title;
+    var had=data.had;
+    var date=data.date;
+var htmlTem =`
+        <html>
+ <head>
+    ${title}
+     <link href="/ui/style.css" rel="stylesheet" />
+   
+</head>
+  <body>
+      <div class="container">
+    <div>
+        <a href="/">Home</a>
+    </div>
+    <hr/>
+    <h3> 
+        ${hed}
+    </h3>
+    <div>
+        ${date}
+    </div> 
+    <div>
+        ${cont}
+    </div>
+    </div>
+  </body>
+</html>
+`;
+  return htmlTem;  
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -13,7 +61,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/artical-1',function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'artical-1.html'));
+   res.send(CreateTemlate(articalOne));
 });
 app.get('/artical-2',function(req,res){
   res.sendFile(path.join(__dirname, 'ui', 'artical-2.html'));
