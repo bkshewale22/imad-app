026ac9de3f3787bed5c2bb22d5141dbdd05ac3f9
bkshewale22 <36422804+bkshewale22@users.noreply.github.com>
2018-03-01 00:25:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,7 +74,14 @@ var htmlTem =`
 `;
   return htmlTem;  
 }
-
+var counter=0;
+app.get('/counter',function(req,res){
+    
+    counter =counter + 1;
+    res.send(counter.toString());
+    
+    
+});
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
