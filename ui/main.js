fd60f3a4ee9edd1c626074be2fb01fcce8b02fc4
bkshewale22 <36422804+bkshewale22@users.noreply.github.com>
2018-02-28 23:51:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,13 @@ var elemnet= document.getElementById('main_text');
 
 
 elemnet.innerHTML ='new value';
+
+var img= document.getElementById('bk')
+
+
+img.onclick=function()
+{
+    
+  img.style.marginLeft='100px';  
+    
+};
