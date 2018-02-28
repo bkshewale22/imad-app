23d71dacc376b4df51d1170497feb91fef57bdf0
bkshewale22 <36422804+bkshewale22@users.noreply.github.com>
2018-02-28 16:52:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ var htmlTem =`
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/main.js', function (req, res) {
+app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
